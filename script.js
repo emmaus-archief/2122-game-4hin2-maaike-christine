6f36e3c2fc94420d3f01cf97dbeadb64b41f86b4
Christine2811 <74548101+Christine2811@users.noreply.github.com>
2022-05-19 12:33:12 +0000

    platformen

--- script.js
+++ script.js
@@ -17,20 +17,42 @@ var spelStatus = SPELEN;
 
 const beweeglinks = 65;
 const beweegrechts = 68;
-const beweegonder = 83;
 const spatiebalk = 32;
 
 /* var vijandX = */
 var spelerX = 30; // x-positie van speler
 var spelerY = 525; // y-positie van speler
-/* var spelerSpringt = false;
-   var snelheid = 10; */
+/*
+var spelerSpringt = false;
+var snelheid = 10; */
 
 platform1X = 0;
 platform1Y = 600;
 platform1W = 200;
 platform1H = 125;
 
+platform2X = 300;
+platform2Y = 500;
+platform2W = 125;
+platform2H = 20;
+
+
+platform3X = 400;
+platform3Y = 390;
+
+platform4X = 565;
+platform4Y = 290;
+
+platform5X = 750;
+platform5Y = 390;
+
+platform6X = 850;
+platform6Y = 500;
+
+platform7X = 1080;
+platform7Y = 600;
+
+ 
 
 /*var img = */
 /* ********************************************* */
@@ -50,17 +72,14 @@ var beweegAlles = function () {
     spelerX = spelerX + 10;
   }
 
-  if(keyIsDown(beweegonder)){
-    spelerY = spelerY + 10;
-  }
- /*
-
+ 
+/*
   // speler springen
 if (spelerSpringt === false && keyIsDown(32)) { // spatie
-    snelheid = 10; 
+    snelheid = 5; 
     spelerSpringt = true;
 }
-if (spelerspringt === true) {
+if (spelerSpringt === true) {
   spelerY = spelerY - snelheid;
   snelheid = snelheid - 0,1;
 }
@@ -70,8 +89,8 @@ if (spelerspringt === true) {
     spelerY = 600;
     
   }
-
-  */
+*/
+  
   
   // vijand
 
@@ -104,14 +123,33 @@ var tekenAlles = function () {
   rect(0,0,1280,720);
   
   // platform
-  fill(119, 79, 39);
+  fill(119, 79, 39); //bruin
   rect(platform1X, platform1Y, platform1W, platform1H);
   fill(79, 107, 24);
-  rect(0, 600, platform1W, 30);
+  rect(platform1X, platform1Y, platform1W, 30); //groen
+
+  // grijze platformen
+  fill(131, 139, 131);
+  rect(platform2X, platform2Y, platform2W, platform2H);
+
+  fill(131, 139, 131);
+  rect(platform3X, platform3Y, platform2W, platform2H);
   
-  var x = 100
-  var y = 100
- 
+  fill(131, 139, 131);
+  rect(platform4X, platform4Y, platform2W, platform2H);
+  
+  fill(131, 139, 131);
+  rect(platform5X, platform5Y, platform2W, platform2H);
+
+  fill(131, 139, 131);
+  rect(platform6X, platform6Y, platform2W, platform2H);
+
+  fill(119, 79, 39); //bruin
+  rect(platform7X, platform7Y, platform1W, platform1H);
+  fill(79, 107, 24);
+  rect(platform7X, platform7Y, platform1W, 30); // groen
+
+  
   // vijand
 
   // speler
